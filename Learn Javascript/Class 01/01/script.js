7ca94053adddef85528fae970e8f91a3d829a80f
torresfelicio <lucastorresfellicio@gmail.com>
2020-06-18 22:38:09 -0300

    estudo javascript

---
+++ Learn Javascript/Class 01/01/script.js
@@ -0,0 +1,9 @@
+function mostraData()
+{
+        document.getElementById("data").innerHTML=Date();
+}
+
+function mostraMensagem()
+{
+        alert('Estou aprendendo a programar em JavaScript!');
+}
---
+++ Learn Javascript/Class 01/02/script.js
@@ -0,0 +1,6 @@
+function chamaNome (){
+
+    document.getElementById("nome").innerHTML= "Lucas";
+  
+  
+}
