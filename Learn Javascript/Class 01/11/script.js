b045ed38f74aa31438acdc12d72a53c9dfa4a63a
torresfelicio <lucastorresfellicio@gmail.com>
2020-06-21 21:36:01 -0300

    adicionado estudos javascript

---
+++ Learn Javascript/Class 01/11/script.js
@@ -0,0 +1,13 @@
+
+function PA(){
+  var valor = parseInt(document.getElementById('inicial').value);
+  var razao = parseInt(document.getElementById('razao').value);
+  var termo = parseInt(document.getElementById('termo').value);
+  var pa='';
+
+  for(var count=1; count<=termo ; count++){
+   pa += "Termo "+count+" = "+valor+"<br />";
+    valor += razao;
+  }
+  document.write(pa);
+}
