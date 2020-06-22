7c512868a8efb019422964646ba243e775398c49
torresfelicio <lucastorresfellicio@gmail.com>
2020-06-21 21:06:07 -0300

    adicionado estudos javascript

---
+++ Learn Javascript/Class 01/08/script.js
@@ -0,0 +1,4 @@
+function exibir(){
+  for(var a=0,  b=10 ; a<=10 &&  b>=0; a++, b--)
+   document.write(a+" - "+b+"<br />");
+}
---
+++ Learn Javascript/Class 01/09/script.js
@@ -0,0 +1,5 @@
+function calcular() {
+  for (var x =0; x<= 10; x++){
+    document.writeln("3 x ",x, "=",x*3,"<br />");
+  }
+}
