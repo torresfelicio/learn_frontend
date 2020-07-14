36dfa9dbb0f6ac7f304dbd5eb66cffdab3fd9494
torresfelicio <lucastorresfellicio@gmail.com>
2020-07-13 22:13:14 -0300

    estudos HTML, CSS, Javascript e backend

---
+++ Learn Javascript/javascriptCeV/aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 100;
+console.log('Detran');
+if (velocidade > 60) {
+  console.log('Você ultrapassou o limite de velocidade MULTADO')
+} else {
+  console.log('Diriga com cuidado')
+}
